7d9f8b118829946f39fdc194b408599fd5f9c8f4
Samuel <sassani134@gmail.com>
2024-04-12 15:41:41 +0200

    pour le prof

--- src/main.cpp
+++ src/main.cpp
@@ -1,165 +1,136 @@
-// arduino library include by default
-#include <Arduino.h>
+#include <Arduino.h>  // Include the Arduino library for Serial
+#include <WiFi.h>  // Librairie Wifi.h
+#include <WebServer.h>  // Librairie WebServer.h
+#define LIGHT_SENSOR_PIN 36 // ESP32 pin GIOP36 (ADC0)  
 
-// Include the ESP32 Wi-Fi library
-// helps connect to the internet
-#include <WiFi.h>
+const char* ssid = "OnePlus 10 Pro 5G";
+const char* password = "nigaznigaz97";
+WebServer server(80);
 
-// Include the ESP32 HTTP client library
-// helps send HTTP requests to the server
-#include <HTTPClient.h>
+// const int led = 5;  // Led sur GPIO5
+// const int Photoresistor = 36;  // Photoresistor sur GPIO34
+bool etatLed = 0;
+char texteEtatLed[2][10] = {"ÉTEINTE","ALLUMÉE"};  // Affichage ETEINTE ou ALLUMEE
 
-// Include the ESP32 Web server library
-// helps create a web server on the ESP32
-#include <WebServer.h>
-
-
-
-// #include <ESP8266WiFi.h>
-// #include <ESP8266HTTPClient.h>
-// #include <ESP8266WebServer.h>
-
-// WIFI information for connection
-// Replace with your network credentials
-const char* ssid = "ssid";
-const char* password = "password";
-
-// Port 80 is used since its default port for HTTP
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds
-const long timeoutTime = 2000;
-
-/* Attaches content and title to a POST request to be sent to Ruby server
- * @params 
- * String content : string value which will be written into
- *                  the post on the ruby server
- * String title : Title of the post made on the website
- */
-void sendToBlog(String content, String title) {
+void handleRoot(){   // Début de la page HTML
+  String page = "<!DOCTYPE html>";
+    page += "<html lang='fr'>";
     
-    HTTPClient http;
+    page += "<head>";
+    page += "    <title>Serveur ESP32</title>";
+    page += "    <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8'/>";
+    page += "    <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";  // Utilisation du css 
+    page += "</head>";
 
-    HTTPClient
-    // Begins HTTP connection to the ruby server
-    http.begin("http://192.168.20.23:3000/posts"); 
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    page += "<body>";
+    page += "    <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>";
+    page += "        <p>ÉTAT LED: "; page += texteEtatLed[etatLed]; page += "</p>";
+    page += "    </div>";
 
-    // Creates POST request string with the custom content attached
-    String postData = "post[title]=" + title + "&post[content]=" + content;
+    page += "    <div class='w3-bar'>";
+    page += "        <a href='/on' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>ON</a>";
+    page += "        <a href='/off' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>OFF</a>";
+    page += "    </div>";
 
-    // Sends POST request to RUBY server
-    int httpCode = http.POST(postData);
-    Serial.println(httpCode);
-        
-    http.end();
+    page += "    <div class='w3-center w3-padding-16'>";
+    page += "        <p>Ce serveur est hébergé sur un ESP32</p>";
+    page += "        <i>Créé par Samuel Assani</i>";
+    page += "        <p>Luminosité: "; page += analogRead(LIGHT_SENSOR_PIN); page += "</p>";
+    page += "    </div>";
+
+    
+
+    page += "</body>";
+    page += "</html>";  // Fin de la page HTML
+
+    server.setContentLength(page.length());  // Permet l'affichage plus rapide après chaque clic sur les boutons
+    server.send(200, "text/html", page);
+}
+
+void handleOn(){  // Code pour allumer la led
+    etatLed = 1;
+    digitalWrite(LED_BUILTIN, HIGH);
+    server.sendHeader("Location","/");
+    server.send(303);
+}
+
+void handleOff(){   // Code pour éteindre la led
+    etatLed = 0;
+    digitalWrite(LED_BUILTIN, LOW);
+    server.sendHeader("Location","/");
+    server.send(303);
+}
+
+void handleNotFound(){  // Page Not found
+  server.send(404, "text/plain","404: Not found");
+}
+
+void photoresistor(){
+  // reads the input on analog pin (value between 0 and 4095)
+  int analogValue = analogRead(LIGHT_SENSOR_PIN);
+
+  Serial.print("Analog Value = ");
+  Serial.print(analogValue);   // the raw analog reading
+
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValue < 40) {
+    Serial.println(" => Dark");
+  } else if (analogValue < 800) {
+    Serial.println(" => Dim");
+  } else if (analogValue < 2000) {
+    Serial.println(" => Light");
+  } else if (analogValue < 3200) {
+    Serial.println(" => Bright");
+  } else {
+    Serial.println(" => Very bright");
+  }
+
+  delay(500);
 }
 
 void setup() {
-  // Init baud rate
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("\n");
 
-  // Init on board LED which is active LOW
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  
-  // Start Connection
+  pinMode(LED_BUILTIN, OUTPUT); // Définition de la led en OUTPUT
+  digitalWrite(LED_BUILTIN, 0);  // Initialisation de la led à 0 (éteinte)
+
+  WiFi.persistent(false);
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  Serial.print("Attente de connexion ...");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
+    delay(100);
   }
-  
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("Connected.");
-  Serial.println("IP address: ");
+
+  Serial.println("\n");
+  Serial.println("Connexion etablie !");
+  Serial.print("Adresse IP: ");
   Serial.println(WiFi.localIP());
 
+  server.on("/", handleRoot);  // Chargement de la page accueil
+  server.on("/on", handleOn);  // Chargement du handleOn - Allumée la led
+  server.on("/off", handleOff);  // Chargement du handleOff - Eteindre la led
+  server.onNotFound(handleNotFound);  // Chargement de la page Not found
   server.begin();
-}
 
-void loop(){
-  // Checks for incoming clients
-  WiFiClient client = server.available();
+  Serial.println("Serveur web actif");
 
-  if (client) {
-    // Reveals new client connection
-    Serial.println("New Client.");
-
-    String currentLine = "";               
-    currentTime = millis();
-    previousTime = currentTime;
-
-    // Loop checks if client is connected and the timout time hasnt expired
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { 
-      
-      currentTime = millis();        
-      
-      if (client.available()) {      
-        // Reads data sent from client and stores in header variable
-        char c = client.read();          
-        Serial.write(c);                    
-        header += c;
-        if (c == '\n') {                
-
-          // Sends client connection info
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/json");
-            client.println("Connection: close");
-            client.println();
-
-            // Checks which route has been requested by the client
-            if (header.indexOf("GET /LED/on") >= 0) {
-              
-              // Turns on LED and sends JSON data to Server
-              Serial.println("Turning LED on");
-              client.println("{\"Rec\": \"LEDon\"}");
-              digitalWrite(LED_BUILTIN, LOW);
-              
-              // Creates new blog post saying LED has been turned on
-              sendToBlog("Turning LED ON", "LED on");
-              
-            } else if (header.indexOf("GET /LED/off") >= 0) {
-              
-              // Turns off LED and sends JSON data to Server
-              Serial.println("Turning LED off");
-              client.println("{\"Rec\": \"LEDoff\"}");
-              digitalWrite(LED_BUILTIN, HIGH);
-              
-              // Creates new blog post saying LED has been turned off
-              sendToBlog("Turning LED OFF","LED off");
-              
-            } 
-            
-          } else { 
-            currentLine = "";
-          }
-        } else if (c != '\r') { 
-          currentLine += c;    
-        }
-        
-      }  
-    }
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+  // find the ip
   
 }
-     
+
+
+void loop() {
+
+    int analogValue = analogRead(LIGHT_SENSOR_PIN);
+
+    server.handleClient();
+    Serial.println(WiFi.localIP());
+    Serial.println("Serveur web actif");
+    delay(10000);
+    photoresistor();
+}
